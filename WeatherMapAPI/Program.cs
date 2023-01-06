76ad440dfd6271c4a272dba2ae713af22789154b
DeAdrien Hill <deadrienhill@yahoo.com>
2023-01-05 20:45:15 -0600

    completed weather api

--- WeatherMapAPI/Program.cs
+++ WeatherMapAPI/Program.cs
@@ -2,7 +2,7 @@
 
 while (true)
 {
-    var apiKey = "4e99177b2c127e79c383e270d2da5803";
+
 
 
     Console.WriteLine("Enter city name");
@@ -10,20 +10,20 @@ while (true)
     var client = new HttpClient();
 
 
-    var weatherMapURL = $"https://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={apiKey}&units=imperial";
+    var weatherMapURL = $"https://api.openweathermap.org/data/2.5/weather?q={cityName}&appid=4e99177b2c127e79c383e270d2da5803&units=imperial";
 
 
     var weatherResponse = client.GetStringAsync(weatherMapURL).Result;
     var weatherSetUp = JObject.Parse(weatherResponse).GetValue("main").ToString();
 
     var temp = JObject.Parse(weatherSetUp).GetValue("temp");
-    
-    
-  
-    
-        
-        Console.WriteLine($"The current Temp is {temp} degrees Fahrenheit in {cityName}");
-    
+
+
+
+
+
+    Console.WriteLine($"The current Temp is {temp} degrees Fahrenheit in {cityName}");
+
 
     Console.WriteLine("Would you like to exit ?");
     var userInput = Console.ReadLine();
